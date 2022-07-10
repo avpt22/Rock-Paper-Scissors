456267d485d76fb67f0f3fc7fde6a9fc708d2755
Angie <angelikavictoria@outlook.com>
2022-07-10 20:35:14 +0200

    Added pseudocode

--- javascript.js
+++ javascript.js
@@ -1,4 +1,37 @@
-    function computerPlay () {
+/* Computer vs player
+
+    Fist to five points win
+
+    If player choose rock and computer choose rock
+        Then tie and no points
+        Play again
+    If player choose scissors and computer choose scissors
+        Then tie and no points
+        Play again
+    If player choose paper and computer choose paper
+        Then tie and no points
+        Play again
+    
+    If player choose rock and computer choose scissors
+        Then player gets one point
+    If player choose paper and computer choose rock
+        Then player gets one point
+    If player choose scissors and computer choose paper
+        Then player gets one point
+
+    If player choose scissors and computer choose rock
+        Then computer gets one point
+    If player choose paper and computer choose scissors
+        Then computer gets one point
+    If player choose rock and computer choose paper
+        Then computer gets one point
+
+    Keep score till computer or player gets five points
+*/
+
+
+
+function computerPlay () {
         const choices = ["rock", "paper", "scissors"];
         let randomChoice = choices[Math.floor(Math.random() * choices.length)];
         
