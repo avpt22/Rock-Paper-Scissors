274f6e4b9ced312e058dbc6210d145fcc01f2735
Angie <angelikavictoria@outlook.com>
2022-07-10 21:48:58 +0200

    Added playerRound function

--- javascript.js
+++ javascript.js
@@ -30,11 +30,10 @@
 */
 
 
+const choices = ["rock", "paper", "scissors"];
 
 function computerPlay () {
-        const choices = ["rock", "paper", "scissors"];
         let computerChoice = Math.floor(Math.random() * choices.length);
-        
         if (computerChoice === 0) {
             return "rock";
         }
@@ -47,3 +46,42 @@ function computerPlay () {
 }
 
 console.log(computerPlay());
+
+const computerSelection = computerPlay();
+let playerSelection = prompt('Rock, paper or scissors?').toLowerCase();
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "rock") {
+        return "That's a tie.";
+    }
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors.";
+    }
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats rock.";
+    }
+    if (playerSelection === "paper" && computerSelection === "paper") {
+        return "That's a tie.";
+    }
+    if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beats paper.";
+    } 
+    if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats rock.";
+    }
+    if (playerSelection === "scissors" && computerSelection === "scissors") {
+        return "That's a tie.";
+    }
+    if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors.";
+    }
+    if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beats paper."
+    }
+}
+
+
+
+
+
+console.log(playRound(playerSelection, computerSelection));
